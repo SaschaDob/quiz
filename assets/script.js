84ff3fd14317512e067cc5e3a5fb31d445ab0f9f
Sascha Dobric <sascha.dobric@gmail.com>
2023-11-22 12:10:53 +0000

    add questions

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const questions = [
+    {question: "What's the capital of Italy?"},
+    {question:" What's the capital of France?"},
+    {question: "What's the capital of Germany?"},
+    {question: "What's the capital of Malta?"},
+];
